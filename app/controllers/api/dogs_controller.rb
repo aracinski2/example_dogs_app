3bd2d91993b8fda7798ef492ef6dff2caf2ab98b
Alex Racinski <aracinski2@gmail.com>
2020-11-18 20:07:54 -0600

    add dog create action

--- app/controllers/api/dogs_controller.rb
+++ app/controllers/api/dogs_controller.rb
@@ -1,2 +1,10 @@
 class Api::DogsController < ApplicationController
+  def create
+    @dog = Dog.new(
+      name: params[:name],
+      age: params[:age],
+      breed: params[:breed]
+    )
+    render 'show.json.jb'
+  end
 end
